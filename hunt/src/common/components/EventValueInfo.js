5bd362f75470a6e94a192999c41a615287f3a638
Tzvetan Marinov <tmarinov@stamus-networks.com>
2021-05-11 18:21:40 +0100

    hunt: virus total links

--- hunt/src/common/components/EventValueInfo.js
+++ hunt/src/common/components/EventValueInfo.js
@@ -16,8 +16,8 @@ const EventValueInfo = (props) => {
         return <a key="onyphe_link" href={`https://www.onyphe.io/search/?query=${props.value}`} target="_blank"> <Icon type="fa" name="info-circle" /></a>;
     }
     return (
-      <OverlayTrigger key="onyphe_link" trigger={['hover', 'hover']} placement="top" overlay={<Tooltip id="tooltip-top">external info</Tooltip>}>
-        <a href={`https://www.onyphe.io/search/?query=${props.value}`} target="_blank">
+      <OverlayTrigger key="virustotal_link" trigger={['hover', 'hover']} placement="top" overlay={<Tooltip id="tooltip-top">external info</Tooltip>}>
+        <a href={`https://www.virustotal.com/gui/ip-address/${props.value}`} target="_blank">
           {' '}
           <Icon type="fa" name="info-circle" />
         </a>
